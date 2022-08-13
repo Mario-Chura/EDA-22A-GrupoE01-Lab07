9f098545c35ac0b2043ce78810f6283f24fd5540
Icielo23 <90292180+Icielo23@users.noreply.github.com>
2022-08-13 13:29:25 -0500

    Clase nodo

--- Nodo.java
+++ Nodo.java
@@ -1,5 +1,17 @@
-package Propuestos;
+
 
 public class Nodo<K, V> {
+  //Atributos
+  K key;
+  V value;
+  
+  //Constructor
+  public Nodo(K key, V value) {
+      this.key = key;
+      this.value = value;
+  }
+  
+  //Setter y Getter
+  
 
 }
