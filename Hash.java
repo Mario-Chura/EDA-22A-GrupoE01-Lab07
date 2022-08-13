45fe0c43559fea04342bd18b6cfa9ec7c58406a9
Fernando Jesus Coyla Alvarez <fcoylaa@unsa.edu.pe>
2022-08-13 14:42:29 -0500

    implementacion de la clase con atributos y constructores

---
+++ Hash.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Hash <K extends String, V extends Integer> implements HashTable<K, V>{
+    // Atributos
+    private final Object[] valores;
+    private int size;
+    private final int lenght;
+
+    //Constructores
+    public Hash(){
+        this(11);
+    }
+
+    public Hash(int lenght) {
+        this.lenght = lenght;
+        valores = new LinkedList[this.lenght];
+    }
+
+}
