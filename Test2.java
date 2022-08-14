deda3af50a828109aa4eaee109fa73c096337496
Mario-Chura <mchurapum@unsa.edu.pe>
2022-08-14 15:25:01 -0500

    Test2

---
+++ Test2.java
@@ -0,0 +1,8 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Test2 {
+	private static final int size = 11;
+	static Hash<String, Integer> hash = new Hash<>(size);
+	static Scanner scan = new Scanner(System.in); 
+}
