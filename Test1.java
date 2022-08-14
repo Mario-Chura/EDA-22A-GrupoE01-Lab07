9096d2e9720125bb3979ca28aa26633fdba66901
Mario-Chura <mchurapum@unsa.edu.pe>
2022-08-14 15:01:20 -0500

    Definimos tamaño de nuestra tabla

---
+++ Test1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class Test1 {
+    private static final int size = 11;
+    static Hash<String, Integer> hash = new Hash<>(size); //Definimos el tamaño de nuestra tabla hash
+    static Scanner scan = new Scanner(System.in)  
+}
